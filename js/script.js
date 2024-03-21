55a4f885d17a46f2dd8e1713b19442158bef7d7d
Wojciech Klause <wojtasxj@gmail.com>
2024-03-21 20:50:31 +0100

    eslint installed

--- js/script.js
+++ js/script.js
@@ -1,61 +1,61 @@
 "use strict";
 
 const titleClickHandler = function(event){
-    console.log('Link was clicked!');
-    console.log(event);
-    
-    const activeLinks = document.querySelectorAll('.titles a.active');
-    for(let activeLink of activeLinks){
-      activeLink.classList.remove('active');
-      }   
-    event.preventDefault();
-    const clickedElement = this;
-      clickedElement.classList.add('active');
-      console.log('clickedElement:', clickedElement);
-
-    const activeArticles = document.querySelectorAll('.posts .active');
-    for(let activeArticle of activeArticles){
-      activeArticle.classList.remove('active');
-      }
+  console.log('Link was clicked!');
+  console.log(event);
   
-    const articleSelector = clickedElement.getAttribute('href');
-    console.log('articleSelector:', articleSelector);
-    
-    const targetArticle = document.querySelector(articleSelector);
-    console.log('targetArticle:', targetArticle);
+  const activeLinks = document.querySelectorAll('.titles a.active');
+  for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }   
+  event.preventDefault();
+  const clickedElement = this;
+  clickedElement.classList.add('active');
+  console.log('clickedElement:', clickedElement);
 
-    targetArticle.classList.toggle('active');
+  const activeArticles = document.querySelectorAll('.posts .active');
+  for(let activeArticle of activeArticles){
+    activeArticle.classList.remove('active');
+  }
 
-}
+  const articleSelector = clickedElement.getAttribute('href');
+  console.log('articleSelector:', articleSelector);
+  
+  const targetArticle = document.querySelector(articleSelector);
+  console.log('targetArticle:', targetArticle);
+
+  targetArticle.classList.toggle('active');
+
+};
   
   
-  /* kolejny submoduł */
-  const optArticleSelector = '.post',
+/* kolejny submoduł */
+const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
   optTitleListSelector = '.titles';
 
-  function generateTitleLinks() {
-    const titleList = document.querySelector(optTitleListSelector);
-    titleList.innerHTML = '';
-    const articles = document.querySelectorAll(optArticleSelector);
-    let html = '';
-    for (let article of articles) {
-      const articleId = article.getAttribute('id');
-      console.log('articleId:', articleId);
-      const articleTitle = article.querySelector(optTitleSelector).innerHTML;
-      console.log('articleTitle:', articleTitle);
-      const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
-      console.log('linkHTML:', linkHTML);
-      html = html + linkHTML;
-    }
-    titleList.innerHTML = html;
-
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-    for(let link of links){
-      link.addEventListener('click', titleClickHandler);
-    }
-  
+function generateTitleLinks() {
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  const articles = document.querySelectorAll(optArticleSelector);
+  let html = '';
+  for (let article of articles) {
+    const articleId = article.getAttribute('id');
+    console.log('articleId:', articleId);
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log('articleTitle:', articleTitle);
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log('linkHTML:', linkHTML);
+    html = html + linkHTML;
   }
-  
-  generateTitleLinks();
+  titleList.innerHTML = html;
+
+  const links = document.querySelectorAll('.titles a');
+  console.log('links:', links);
+  for(let link of links){
+    link.addEventListener('click', titleClickHandler);
+  }
+
+}
+
+generateTitleLinks();
